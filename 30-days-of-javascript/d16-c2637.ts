c7682b842d4c7f903a742b2651c19b91f170b153
Alwisteins <alwi.wahyu.w@gmail.com>
2026-02-14 16:12:13 +0700

    day 16: learn promise & time

---
+++ 30-days-of-javascript/d16-c2637.ts
@@ -0,0 +1,16 @@
+type Fn2 = (...params: any[]) => Promise<any>;
+
+function timeLimit(fn: Fn2, t: number): Fn {
+  return async function (...args) {
+    const failed = new Promise((resolve, reject) => {
+      setTimeout(() => reject("Time Limit Exceeded"), t);
+    });
+
+    return Promise.race([failed, fn(...args)]).then((value) => value);
+  };
+}
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
