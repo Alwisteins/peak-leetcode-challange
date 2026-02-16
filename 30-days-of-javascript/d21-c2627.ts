5dcbc5a8108d9037fbe9e75ae756d83a0ba45f81
Alwisteins <alwi.wahyu.w@gmail.com>
2026-02-16 19:53:15 +0700

    day 23: time to back to fundamental

---
+++ 30-days-of-javascript/d21-c2627.ts
@@ -0,0 +1,7 @@
+function chunk(arr: Obj[], size: number): Obj[][] {
+  let chunked = [];
+  for (let i = 0; i < arr.length; i += size) {
+    chunked.push(arr.slice(i, i + size));
+  }
+  return chunked;
+}
---
+++ 30-days-of-javascript/d23-c2631.ts
@@ -0,0 +1,23 @@
+interface Array<T> {
+  groupBy(fn: (item: T) => string): Record<string, T[]>;
+}
+
+Array.prototype.groupBy = function (fn) {
+  let grouped: any = {};
+  if (this.length === 0) return grouped;
+
+  for (let i = 0; i < this.length; i++) {
+    let key = fn(this[i]);
+    let exist = grouped[key];
+    if (exist) {
+      grouped[key] = [...exist, this[i]];
+    } else {
+      grouped[key] = [this[i]];
+    }
+  }
+  return grouped;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
